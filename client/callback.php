d432fa699619a1b20a07d8418396558cee963baf
Masahiro Ide <imasahiro9@gmail.com>
2013-02-22 04:16:20 +0900

    add twitteroath

---
+++ client/callback.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once('config.php');
+require_once('utils.php');
+require_once('twitteroauth/twitteroauth.php');
+
+session_start();
+
+$oauth = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
+
+$accessToken = $oauth->getAccessToken($_GET['oauth_verifier']);
+
+$me = $oauth->get('account/verify_credentials');
+
+if (empty($user)) {
+    $user = mysql_fetch_assoc($rs);
+}
+
+session_regenerate_id(true);
+$_SESSION['user'] = array(
+        'twitter_screen_name' => ($me->screen_name),
+        'twitter_profile_image_url' => ($me->profile_image_url),
+);
+
+jump('/');
+
+?>
